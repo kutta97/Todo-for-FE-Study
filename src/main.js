c32eeffa05bb7422d48d627d90850fc9d686135a
kutta97 <yhjin04170@gmail.com>
2022-02-09 15:37:04 +0900

    toDoItem 컴포넌트 추가

--- src/main.js
+++ src/main.js
@@ -1,23 +1,43 @@
-"use strcit";
+import Todo from "./todo.js";
 
 const ENTER_KEY = 13;
 const currentList = document.querySelector(".todo-list");
 const input = document.querySelector(".todo-input");
 
+let todos = [];
+let id = 0;
+
 input.addEventListener("keyup", enterkey, false);
 
 function enterkey(event) {
   if (event.keyCode == ENTER_KEY) {
-    if (!input.value) return;
+    const title = input.value.trim();
+    if (!title) return;
 
-    addTodo(input.value);
+    addTodo(title);
     input.value = null;
   }
 }
 
-function addTodo(value) {
-  const newList = document.createElement("li");
+const toDoItem = function (todos) {
+  return `
+		${todos
+      .map(function (todo) {
+        return `
+          <li data-id="${todo.id}" class="todo-item">
+            <input class="complete" type="checkbox" false />
+            <label>${todo.title}</label>
+            <button class="delete">X</button>
+          </li>`;
+      })
+      .join("")}
+  `;
+};
 
-  newList.appendChild(document.createTextNode(value));
-  currentList.appendChild(newList);
+function addTodo(value) {
+  const newTodo = new Todo(id, value);
+  todos.push(newTodo);
+  currentList.innerHTML = toDoItem(todos);
+
+  id++;
 }
