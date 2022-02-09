3465ec46adfa5763f6efa84eb201edf227447f7a
kutta97 <yhjin04170@gmail.com>
2022-02-09 13:52:23 +0900

    할 일 입력 addTodo()  추가

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const ENTER_KEY = 13;
+const currentList = document.querySelector(".todo-list");
+const input = document.querySelector(".todo-input");
+
+input.addEventListener("keyup", enterkey, false);
+
+function enterkey(event) {
+  if (event.keyCode == ENTER_KEY) {
+    if (!input.value) return;
+
+    addTodo(input.value);
+    input.value = null;
+  }
+}
+
+function addTodo(value) {
+  const newList = document.createElement("li");
+
+  newList.appendChild(document.createTextNode(value));
+  currentList.appendChild(newList);
+}
